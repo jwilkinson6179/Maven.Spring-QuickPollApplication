49e8ae107e4a30c35a8c86d4b6e9812c653b81bc
jwilkinson6179 <JamesW6179@gmail.com>
2020-03-28 02:10:04 -0400

    Fixed errors on disconnect, still won't load import.sql

---
+++ src/main/resources/import.sql
@@ -0,0 +1,135 @@
+INSERT INTO poll (poll_id, question) 
+VALUES (3, 'What is your favorite color?');
+INSERT INTO poll (poll_id, question) 
+VALUES (4, 'What is your least favorite color?');
+INSERT INTO poll (poll_id, question) 
+VALUES (5, 'What is your favorite movie?');
+INSERT INTO poll (poll_id, question) 
+VALUES (6, 'What is your least favorite movie?');
+INSERT INTO poll (poll_id, question) 
+VALUES (7, 'What is your favorite book?');
+INSERT INTO poll (poll_id, question) 
+VALUES (8, 'What is your lease favorite book?');
+INSERT INTO poll (poll_id, question) 
+VALUES (9, 'What is your favorite food?');
+INSERT INTO poll (poll_id, question) 
+VALUES (10, 'What is your least favorite food?');
+INSERT INTO poll (poll_id, question) 
+VALUES (11, 'What is your favorite drink?');
+INSERT INTO poll (poll_id, question) 
+VALUES (12, 'What is your least favorite drink?');
+INSERT INTO poll (poll_id, question) 
+VALUES (13, 'What is your favorite athlete?');
+INSERT INTO poll (poll_id, question) 
+VALUES (14, 'What is your least favorite athlete?');
+INSERT INTO poll (poll_id, question) 
+VALUES (15, 'What is your favorite music genre?');
+INSERT INTO poll (poll_id, question) 
+VALUES (16, 'What is your least favorite music genre?');
+INSERT INTO poll (poll_id, question) 
+VALUES (17, 'What is your favorite city?');
+
+INSERT INTO options (options_id, options_value, poll_id) 
+VALUES (7, 'Red', 3);
+INSERT INTO options (options_id, options_value, poll_id) 
+VALUES (8, 'Blue', 3);
+INSERT INTO options (options_id, options_value, poll_id) 
+VALUES (9, 'Yellow', 3);
+
+INSERT INTO options (options_id, options_value, poll_id) 
+VALUES (10, 'Red', 4);
+INSERT INTO options (options_id, options_value, poll_id) 
+VALUES (11, 'Blue', 4);
+INSERT INTO options (options_id, options_value, poll_id) 
+VALUES (12, 'Yellow', 4);
+
+INSERT INTO options (options_id, options_value, poll_id) 
+VALUES (13, 'Star Wars', 5);
+INSERT INTO options (options_id, options_value, poll_id) 
+VALUES (14, 'The Matrix', 5);
+INSERT INTO options (options_id, options_value, poll_id) 
+VALUES (15, 'The Avengers', 5);
+
+INSERT INTO options (options_id, options_value, poll_id) 
+VALUES (16, 'Star Wars', 6);
+INSERT INTO options (options_id, options_value, poll_id) 
+VALUES (17, 'The Matrix', 6);
+INSERT INTO options (options_id, options_value, poll_id) 
+VALUES (18, 'The Avengers', 6);
+
+INSERT INTO options (options_id, options_value, poll_id) 
+VALUES (19, 'Bible', 7);
+INSERT INTO options (options_id, options_value, poll_id) 
+VALUES (20, 'One Hundred Years Of Solitude', 7);
+INSERT INTO options (options_id, options_value, poll_id) 
+VALUES (21, 'Game of Thrones', 7);
+
+INSERT INTO options (options_id, options_value, poll_id) 
+VALUES (22, 'Bible', 8);
+INSERT INTO options (options_id, options_value, poll_id) 
+VALUES (23, 'One Hundred Years of Solitude', 8);
+INSERT INTO options (options_id, options_value, poll_id) 
+VALUES (24, 'Game of Thrones', 8);
+
+INSERT INTO options (options_id, options_value, poll_id) 
+VALUES (25, 'Pizza', 9);
+INSERT INTO options (options_id, options_value, poll_id) 
+VALUES (26, 'French fries', 9);
+INSERT INTO options (options_id, options_value, poll_id) 
+VALUES (27, 'Ice cream', 9);
+
+INSERT INTO options (options_id, options_value, poll_id) 
+VALUES (28, 'Pizza', 10);
+INSERT INTO options (options_id, options_value, poll_id) 
+VALUES (29, 'French fries', 10);
+INSERT INTO options (options_id, options_value, poll_id) 
+VALUES (30, 'Ice cream', 10);
+
+INSERT INTO options (options_id, options_value, poll_id) 
+VALUES (31, 'Beer', 11);
+INSERT INTO options (options_id, options_value, poll_id) 
+VALUES (32, 'Wine', 11);
+INSERT INTO options (options_id, options_value, poll_id) 
+VALUES (33, 'Cola', 11);
+
+INSERT INTO options (options_id, options_value, poll_id) 
+VALUES (34, 'Beer', 12);
+INSERT INTO options (options_id, options_value, poll_id) 
+VALUES (35, 'Wine', 12);
+INSERT INTO options (options_id, options_value, poll_id) 
+VALUES (36, 'Cola', 12);
+
+INSERT INTO options (options_id, options_value, poll_id) 
+VALUES (37, 'LeBron James', 13);
+INSERT INTO options (options_id, options_value, poll_id) 
+VALUES (38, 'Pete Rose', 13);
+INSERT INTO options (options_id, options_value, poll_id) 
+VALUES (39, 'Barry Bonds', 13);
+
+INSERT INTO options (options_id, options_value, poll_id) 
+VALUES (40, 'LeBron James', 14);
+INSERT INTO options (options_id, options_value, poll_id) 
+VALUES (41, 'Pete Rose', 14);
+INSERT INTO options (options_id, options_value, poll_id) 
+VALUES (42, 'Barry Bonds', 14);
+
+INSERT INTO options (options_id, options_value, poll_id) 
+VALUES (43, 'Rap', 15);
+INSERT INTO options (options_id, options_value, poll_id) 
+VALUES (44, 'Country', 15);
+INSERT INTO options (options_id, options_value, poll_id) 
+VALUES (45, 'Rock', 15);
+
+INSERT INTO options (options_id, options_value, poll_id) 
+VALUES (46, 'Rap', 16);
+INSERT INTO options (options_id, options_value, poll_id) 
+VALUES (47, 'Country', 16);
+INSERT INTO options (options_id, options_value, poll_id) 
+VALUES (48, 'Rock', 16);
+
+INSERT INTO options (options_id, options_value, poll_id) 
+VALUES (49, 'New York', 17);
+INSERT INTO options (options_id, options_value, poll_id) 
+VALUES (50, 'Pittsburgh', 17);
+INSERT INTO options (options_id, options_value, poll_id) 
+VALUES (51, 'San Francisco', 17);
